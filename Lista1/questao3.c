2764cc7c0a622e2078acc5b41df9855918cf5ab2
Arley Prates <arleyprates@gmail.com>
2014-04-12 23:20:01 -0300

    Criando pasta

---
+++ Lista1/questao3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	int x;
+	printf ("\nInforme um numero qualquer: ");
+	scanf ("%d", &x);
+	printf ("\nO numero informado foi: %d \n", x);
+	return 0;
+}
