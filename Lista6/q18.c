45e4ccbc813c7e642c1447f6affd8efc39f11ef5
Arley Prates <arleyprates@gmail.com>
2014-07-17 00:06:50 -0300

    Ajustando 18

--- Lista6/q18.c
+++ Lista6/q18.c
@@ -6,16 +6,14 @@ void main()
   int i;
   int result;
   clrscr();
-  arq = fopen("ArqGrav.txt", "wt");  // Cria um arquivo texto para gravação
-  if (arq == NULL) // Se nào conseguiu criar
+  arq = fopen("ArqGrav.txt", "wt"); 
+  if (arq == NULL) 
   {
      printf("Problemas na CRIACAO do arquivo\n");
      return;
   }
   for (i = 0; i<10;i++)
   {
-// A funcao 'fprintf' devolve o número de bytes gravados 
-// ou EOF se houve erro na gravação
       result = fprintf(arq,"Linha %d\n",i);  					  
       if (result == EOF)		    
 	  printf("Erro na Gravacao\n");
