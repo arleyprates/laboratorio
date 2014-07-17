7ebff1eb737b89b298eaf120dc8b4b70a858265b
Arley Prates <arleyprates@gmail.com>
2014-07-16 22:22:03 -0300

    Add q03

---
+++ Lista6/q03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+	
+int main (void ) {
+      int i = 0;
+      char linha[100];
+      FILE *arquivo;
+      char nome[20];
+      printf( "Digite nome do arquivo desejado arquivora saber quantoas linhas tem. Ex: a.txt\n");
+      scanf ("%s", &nome);
+      if ((arquivo = fopen (nome, "r")) == NULL) {
+            printf("\nErro ao abrir o arquivo.\n");
+            exit(1);
+      }
+      while (!feof (arquivo)) {
+            fgets (linha, 100, arquivo);
+            i++;
+      }
+      printf ("O arquivo tem %d linhas", i);
+      fclose (arquivo);
+      getchar ();
+}
