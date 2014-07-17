3d089aa16a8e3744b5c685abd23d8cb742196e7c
Arley Prates <arleyprates@gmail.com>
2014-07-16 23:32:16 -0300

    Add q06

---
+++ Lista6/q06.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+	char NomeArquivo[200];
+	char palavra[20];
+	FILE *arquivo1;
+
+	int conta1 = 0, repete = 0;
+	int tam = strlen (palavra);
+	char prox;
+
+	printf ("Informe nome/endereco do arquivo1: ");
+	scanf ("%s", &NomeArquivo);
+	arquivo1 = fopen (NomeArquivo,"r");
+
+	if (arquivo1 == NULL) {
+		printf ("Nao foi possivel criar/abrir o arquivo.\n");
+		exit (1);
+	}
+	printf ("Palavra a pesquisar: ");
+	scanf ("%s", &palavra);
+	while (conta1 == 0){
+		prox = fgetc (arquivo1);
+		if (prox == EOF)
+			break;
+		if (prox == palavra[conta1]){
+			conta1++;
+			if (conta1 == tam){
+				conta1 = 0;
+				repete++;
+			}
+		}
+		else
+			conta1 = 0;
+	}
+	printf("A palavra '%s' se repete %d vezes\n", palavra, repete);
+	fclose (arquivo1); 
+	return 0;
+}
