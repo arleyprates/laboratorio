e5d7138436bc2eacb771c1061aa11875d93854ad
Arley Prates <arleyprates@gmail.com>
2014-07-17 00:03:45 -0300

    Ajustando q14.c

--- Lista6/q14.c
+++ Lista6/q14.c
@@ -3,26 +3,18 @@
  
 int main(void)
 {
-  FILE *pont_arq; // cria variável ponteiro para o arquivo  
-  char palavra[20]; // variável do tipo string
-   
-  //abrindo o arquivo com tipo de abertura w
+  FILE *pont_arq;   
+  char palavra[20]; 
   pont_arq = fopen("arquivo_palavra.txt", "w"); 
    
-  //testando se o arquivo foi realmente criado
   if(pont_arq == NULL)
   {
      printf("Erro na abertura do arquivo!");
      return 1; 
-  }
-     
+  }  
   printf("Escreva uma palavra para testar gravacao de arquivo: ");  
   scanf("%s", palavra);
-   
-  //usando fprintf para armazenar a string no arquivo
   fprintf(pont_arq, "%s", palavra); 
-   
-  //usando fclose para fechar o arquivo
   fclose(pont_arq);  
      
   printf("Dados gravados com sucesso!");
